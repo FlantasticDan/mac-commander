fc32cb8a4ff6e37dc26c16169eaeef4670564959
Daniel Flanagan <dancharley8@gmail.com>
2021-08-13 00:31:46 -0700

    Basic Reporting

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-from flask import Flask
+import time
+
+from flask import Flask, render_template, request
 from deta import Deta
+
+from util import normalize_mac_address
 
 app = Flask(__name__)
 
 deta = Deta()
-db = deta('MACs')
+status = deta.Base('Status')
+command = deta.Base('Command')
 
+@app.get('/mac/<mac_address>')
+def mac_summary(mac_address):
+    state = status.get(normalize_mac_address(mac_address, False))
+    if state:
+        return render_template('mac.html', mac=normalize_mac_address(mac_address), **state)
+    return render_template('mac.html', mac=normalize_mac_address(mac_address), local='Unknown', public='Unknown', timestamp='Never')
+
+@app.post('/ping')
+def incoming_ping():
+    payload = request.json
+    summary = {
+        'key': normalize_mac_address(payload['mac'], display=False),
+        'local': payload['local'],
+        'public': payload['public'],
+        'timestamp': time.time()
+    }
+    status.put(summary)
+    return 'OK'
